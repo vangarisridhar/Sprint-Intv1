65feeaa285a77404b2b8b323287c7f070b827f04
r v subrahmanyam istamsetty <vangarisridhar@gmail.com>
2024-02-04 17:00:27 +0530

    search amazon automation code

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -5,40 +5,35 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-
 public class TestCases {
     ChromeDriver driver;
-    public TestCases()
-    {
+
+    public TestCases() {
         System.out.println("Constructor: TestCases");
         WebDriverManager.chromedriver().timeout(30).setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
 
-    public void endTest()
-    {
+    public void endTest() {
         System.out.println("End Test: TestCases");
         driver.close();
         driver.quit();
 
     }
 
-    
-    public  void testCase01(){
+    public void testCase01() {
         System.out.println("Start Test case SearchAmazon: testCase01");
-      
-         // Navigate to google https://www.google.com/
-       SearchAmazon sa = new SearchAmazon(driver);
-         if(sa.searchForAmazon())
-            System.out.println("Testcase passed");
-         else
-         System.out.println("Testcase failed");
 
+        // Navigate to google https://www.google.com/
+        SearchAmazon sa = new SearchAmazon(driver);
+        if (sa.searchForAmazon())
+            System.out.println("Testcase passed");
+        else
+            System.out.println("Testcase failed");
 
         System.out.println("end Test case SearchAmazon: testCase01");
-   
-    }
 
+    }
 
 }
