824f28501e43b0afa53789159ea6f043d80ec0b0
sridhar <sridhar_vangari@epam.com>
2024-06-06 06:16:23 +0530

    pushing maven code

---
+++ Mavenhometask2/src/test/java/hometask1/AppTest.java
@@ -0,0 +1,26 @@
+package hometask1;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        Assert.assertTrue( true );
+    }
+    
+    @Test
+    public void shouldAnswerWithFalse()
+    {
+        Assert.assertFalse( false );
+    }
+}
+
---
+++ Mavenhometask2/src/test/java/hometask1/Testing.java
@@ -0,0 +1,13 @@
+package hometask1;
+
+import org.testng.annotations.Test;
+
+public class Testing {
+	
+	@Test
+	public void print() {
+		
+		System.out.println("Test.java got executed");
+	}
+
+}
